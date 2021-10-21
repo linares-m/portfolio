e04770d629740954fae6e3b3c0f7aa5d02f76b6d
linares-m <mlinaresoftexas@gmail.com>
2021-10-21 13:26:39 -0500

    Added details to projects
    Signed-off-by: linares-m <mlinaresoftexas@gmail.com>

--- portfolioResponsive.js
+++ portfolioResponsive.js
@@ -1,11 +1,13 @@
 //Intro Scripts
 window.addEventListener("DOMContentLoaded", intro);
 window.addEventListener("resize", intro);
+// window.addEventListener("DOMContentLoaded", colorGridFunctions);
+// window.addEventListener("resize", colorGridFunctions);
 function intro(){
   if (window.matchMedia("(max-width: 700px)").matches) {
-    setTimeout (mobileIntro, 2000)
+    setTimeout (mobileIntro, 800)
     function mobileIntro (){
-      console.log("window screen smaller than 700px")
+      console.log("device width smaller than 700px")
       document.getElementById("header").style.width = "100vw";
       document.getElementById("slider").style.width = "100vw";
       document.getElementById("main").style.display = "grid";
@@ -18,18 +20,44 @@ function intro(){
     }
   }
   if (window.matchMedia("(min-width: 700px)").matches) {
-    setTimeout (desktopIntro, 2000)
+    setTimeout (desktopIntro, 800)
     function desktopIntro() {
       document.getElementById("header").style.width = "40vw";
       document.getElementById("slider").style.width = "min-content";
       document.getElementById("slider").style.display = "block";
       document.getElementById("main").style.display = "flex";
       document.getElementById("navContainer").style.visibility = "visible";
-      document.getElementById("title").style.transform = "translateY(140px)";
-      document.getElementById("head").style.transform = "translateY(140px)";
+      document.getElementById("title").style.transform = "translateY(0px)";
+      document.getElementById("head").style.transform = "translateY(0px)";
       document.getElementById("aboutP").style.display = "block";
     }
+
   }
+  // setTimeout(colorGridFunctions, 1000);
+  // function colorGridFunctions(){
+  //   function getRndInteger(min, max) {
+  //     return Math.floor(Math.random() * (max - min + 1) ) + min;
+  //   }
+  //   function getRndColumn(min, max) {
+  //     return (Math.floor(Math.random() * (max - min + 1) ) + min);
+  //   }
+  //   if (window.matchMedia("(min-width: 700px)").matches){
+  //     var rectSelect = document.getElementsByTagName('rect');
+  //     console.log(rectSelect)
+  //     for(i=0; i<rectSelect.length; i++){
+  //       rectSelect[i].addEventListener('mouseover', addColorEvent);
+  //       function addColorEvent(){
+  //         this.style.fill= 'hsla(' + getRndInteger(0, 300) + ',100%,50%,1)';
+  //       };
+  //     }
+  //     setInterval(blinkingColors, 500);
+  //     function blinkingColors(){
+  //       for(i=0; i<rectSelect.length; i++){
+  //         rectSelect[i].style.fill= 'hsla(' + getRndInteger(0, 300) + ',100%,50%,.65)';
+  //       };
+  //     }
+  //   }
+  // }
 
   //About Section Script
   document.getElementById("aboutLink").addEventListener("click", aboutO);
@@ -57,25 +85,23 @@ function intro(){
   }
 }
 
-
-
-  //Accordion/Panel: Open/Close
+//Accordion/Panel: Open/Close
 window.addEventListener("DOMContentLoaded", accordionFunctions);
 function accordionFunctions(){
-var acc = document.getElementsByClassName("accordion");
-let i;
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let lipan = this.nextElementSibling;
+  var acc = document.getElementsByClassName("accordion");
+  let i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let lipan = this.nextElementSibling;
 
-    if (lipan.style.display === "block") {
-      lipan.style.display = "none";
-    } else {
-      lipan.style.display = "block";
-    }
-  });
-}
+      if (lipan.style.display === "block") {
+        lipan.style.display = "none";
+      } else {
+        lipan.style.display = "block";
+      }
+    });
+  }
   let accH = document.getElementsByClassName("accordionHeader");
   let iH;
   for (iH = 0; iH < accH.length; iH++) {
